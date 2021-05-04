2261ca96f33f5ce164359b8999ff32837b53e37c
David Moncayo <dkmoncayo@gmail.com>
2021-05-03 22:06:31 -0400

    Forgot something

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -6,30 +6,11 @@ import BuyButton from './BuyButton';
 
 require('dotenv').config();
 
-const { ethers } = require('ethers');
-
-const pawnchainAbi = require('../abi/Pawnchain.json').abi
-
 const Preview = props => {
     const dispatch = useDispatch()
     const accountStatus = useSelector(state => state.accountStatus);
     const { currentAccount } = accountStatus;
     const { token, tokenID } = props;
-
-    const buyToken = () => {
-        window.ethereum.request({
-            method: 'eth_sendTransaction',
-            params: [
-                {
-                    from: currentAccount,
-                    to: process.env.REACT_APP_CONTRACT_ADDRESS,
-                    value: ethers.utils.parseEther(token.price).toHexString(),
-                    data: new ethers.utils.Interface(pawnchainAbi).encodeFunctionData('vendingMachine', [tokenID])
-                }
-            ]
-        })
-        .catch(e => console.log(e));
-    }
     
     return (
         <div className="gif-wrapper">
