cc47abc3fedaffbddde30d9026806d393e0b357b
David Moncayo <david@stablesoftworks.com>
2021-05-08 14:46:46 -0400

    mobile friendly

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { account } from '../actions/accountActions';
 import Metamask_icon from '../metamask-logo2.png'
 
-const ethereum = window.ethereum;
+const { ethereum } = window;
 
 const Navbar = () => {
     const dispatch = useDispatch();
@@ -17,14 +17,16 @@ const Navbar = () => {
             .then(wallet => dispatch(account(wallet[0])))
     }
 
-    ethereum.on('accountsChanged', function (accounts) {
-        dispatch(account(accounts[0]));
-    });
+    if (ethereum) {
+        ethereum.on('accountsChanged', function (accounts) {
+            dispatch(account(accounts[0]));
+        });    
+    }
 
     return (
         <div className="nav-wrapper">
             <h1 className="logo-text">Pawnchain</h1> 
-            { currentAccount ? <h1 className="address-container">{currentAccount}</h1> : <div className="connect-button-wrapper"> <button className="connect-button" onClick={connectWallet}>Connect<img className="metamask-icon" src={Metamask_icon} alt="metamask-logo.png"/></button></div> }
+            { currentAccount ? <h1 className="address-container">{currentAccount}</h1> : <div className="connect-button-wrapper"> <button disabled={!ethereum} className="connect-button" onClick={connectWallet}>Connect<img className="metamask-icon" src={Metamask_icon} alt="metamask-logo.png"/></button></div> }
         </div>
     );
 }
